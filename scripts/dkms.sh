b5693fdd329df72224e2a7615196dfb017aab5b2
Andrii Luchkovych <233andre10090@gmail.com>
2025-10-07 16:32:43 +0300

    edit dkms.sh

--- scripts/dkms.sh
+++ scripts/dkms.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get install -y --no-install-recommends software-properties-common apt-transport-https ca-certificates gnupg
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends software-properties-common apt-transport-https ca-certificates gnupg
 
-add-apt-repository -y universe
-add-apt-repository -y multiverse
-apt-get update
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y multiverse
+sudo apt-get update
